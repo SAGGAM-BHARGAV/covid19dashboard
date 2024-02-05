5c54864d8d22f565abab78a2529b6c254f74edc4
SAGGAM BHARGAV <bhargavsaggam@gmail.com>
2024-02-05 19:36:33 +0000

    covid19dashboard

---
+++ src/components/Footer/index.js
@@ -0,0 +1,37 @@
+import {VscGithubAlt} from 'react-icons/vsc'
+import {FaLinkedin, FaTwitter} from 'react-icons/fa'
+
+import './index.css'
+
+const Footer = () => (
+  <div className="footer-container">
+    <h1 className="footer-heading">
+      COVID19
+      <span className="higlight-india">INDIA</span>
+    </h1>
+    <p className="footer-description">
+      we stand with everyone fighting on the front lines
+    </p>
+    <div className="footer-icons-container">
+      <a
+        aria-label="label"
+        href="https://github.com/mubashir0356/Covid-19-Dashboard"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <VscGithubAlt className="github-icon" />
+      </a>
+      <a
+        aria-label="label"
+        href="https://www.linkedin.com/in/abdul-mubashir-fullstack-developer/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaLinkedin className="instagram-icon" />
+      </a>
+      <FaTwitter className="twitter-icon" />
+    </div>
+  </div>
+)
+
+export default Footer
